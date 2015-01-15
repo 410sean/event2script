ff33b9321d5f869392dc1720d79d09f63b402f76
410sean <410sean@gmail.com>
2015-01-15 17:29:17 -0500

    initial build

---
+++ eventhandler.ps1
@@ -0,0 +1,14 @@
+﻿param($eventID,$subjectusername,$targetusername,$targetsid,$subjectusersid)
+
+function event-passreset ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " attempted to reset the password for " + $targetusername + " at " + (get-date) | out-file C:\scripts\output.var -Append
+}
+function event-membershipchange ()
+{
+
+}
+if ($eventID -eq 4724)
+{
+    event-passreset -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
